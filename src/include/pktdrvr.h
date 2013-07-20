dbc6883d6b2459d0404c9f9a929549a76093f6e5
Stas Sergeev <stsp@users.sourceforge.net>
2013-07-21 00:48:18 +0400

    change the reference to COPYING.DOSEMU with an explicit GPLv2 heading

--- src/include/pktdrvr.h
+++ src/include/pktdrvr.h
@@ -1,7 +1,18 @@
 /*
- * (C) Copyright 1992, ..., 2007 the "DOSEMU-Development-Team".
+ *  (C) Copyright 1992, ..., 2007 the "DOSEMU-Development-Team".
  *
- * for details see file COPYING.DOSEMU in the DOSEMU distribution
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, version 2, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #ifndef PKTDRVR_H
