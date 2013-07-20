b0c0830292330e08df498063a78cb67c99a91017
Stas Sergeev <stsp@users.sourceforge.net>
2013-07-21 00:55:20 +0400

    replace more references to COPYING.DOSEMU with explicit heading

--- src/env/video/svgalib.c
+++ src/env/video/svgalib.c
@@ -2,10 +2,20 @@
  * written March/April 2000 (original code)
  * Bart Oldeman <bart.oldeman@bristol.ac.uk>
  * with help from Matan Ziv-Av <matan@svgalib.org>
- * All modifications in this file to the original code are
- * (C) Copyright 1992, ..., 2007 the "DOSEMU-Development-Team".
+ *  (C) Copyright 1992, ..., 2007 the "DOSEMU-Development-Team".
  *
- * for details see file COPYING.DOSEMU in the DOSEMU distribution
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, version 2, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
 /* Interface for svgalib.
